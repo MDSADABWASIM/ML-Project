1d139ebf71049621e0284e2f859daaf50c0e8346
md sadab wasim <mdsadabwasim007@gmail.com>
2020-03-31 11:22:47 +0530

    Day 86 OCR using Tesseract and Tessy library

---
+++ Code/Day86-TesseractProject.py
@@ -0,0 +1,97 @@
+import tessy
+import cv2
+import numpy as np
+
+# Path to an image
+imagePath = r'/Users/inndata_flutter/Downloads/sample1.png'
+
+# Inits the module (optional - see the examples)
+tessy.init()
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY )
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+image = cv2.imread(imagePath)
+
+gray = get_grayscale(image)
+thresh = deskew(gray)
+
+# Gets the text from the image
+text = tessy.image_to_string(thresh,lang='eng')
+
+# Shows the text in the console
+print(text)
+
+
+# FOR SHOWING THE CLEANING IMAGES IN STAGES
+
+# import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+# imagePath = r'/Users/inndata_flutter/Downloads/sample4.jpg'
+
+
+# img = cv2.imread(imagePath,0)
+# ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+# ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+# ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+# ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+# titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+# images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+# for i in range(4):
+#     plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+#     text = tessy.image_to_string(images[i],lang='mya')
+#     print(titles[i]+'\n')
+#     print(text)
+# plt.show()
